24a0a055bd9c1d6687519e9f67270bcb1efa6c66
Varsha Muzumdar <varsha.vjm@gmail.comm>
2019-03-19 22:51:25 -0700

    class 7 computed members changes added

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ var app = new Vue ({
 
 	el: '#app',
 	data: {
+		brand: 'Victors Original',
 		product : 'Socks',
-		image : "./assets/vmsock-green.jpg",
+		selectedVariant : 0,
 		SockURL: "http://google.com",
 		inStock: 12,
-		Toggle: false,
 		sockColor:[
-		{sockValue:"Sock Color: red", colour:"red", sockImage :"./assets/vmsock-red.png"},
-		{sockValue:"Sock Color: blue", colour:"blue", sockImage :"./assets/vmsock-blue.jpg"},
-		{sockValue:"Sock Color: green", colour:"green", sockImage :"./assets/vmsock-green.jpg"}],
+		{sockValue:"Sock Color: red", colour:"red", sockImage :"./assets/vmsock-red.png", quantity:0},
+		{sockValue:"Sock Color: blue", colour:"blue", sockImage :"./assets/vmsock-blue.jpg", quantity:13},
+		{sockValue:"Sock Color: green", colour:"green", sockImage :"./assets/vmsock-green.jpg", quantity:12}],
 		cart:0,
 	},
 	methods: {
@@ -20,9 +20,23 @@ var app = new Vue ({
 			this.cart =this.cart + 1
 		},
 
-		updateImage: function(p)
+		updateImage: function(index)
 		{
-			this.image = p.sockImage;
+			this.selectedVariant = index;
+			console.log(this.selectedVariant);
+		}
+	},
+	computed:{
+		title ()
+		{
+			return this.brand + ' '+ this.product;
+		},
+		image(p){
+			return this.sockColor[this.selectedVariant].sockImage;
+		},
+		cartToggler()
+		{
+			return this.inStock == this.sockColor[this.selectedVariant].quantity;
 		}
 	}
 
